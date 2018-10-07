3ad70e6f280e2294101389b3cecd935b7bbaa8f9
Ana Harris <vana1990@gmail.com>
2018-10-07 05:25:46 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |el|
-    el.split("").map do |x|
+    el.split("").collect do |x|
       alphabet.index(x)
     end
   end
